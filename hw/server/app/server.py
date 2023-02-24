ad54bcc5e6980bb46aa883aceb98b6aa3d59231f
mohammadm <mohammadm@uvic.ca>
2023-02-23 17:43:27 -0800

    clean up

--- hw/server/app/server.py
+++ hw/server/app/server.py
@@ -12,24 +12,29 @@ def server():
             if('latitude' in parsed_path.query and 'longitude' in parsed_path.query):
                 latitude = parse_qs(parsed_path.query).get('latitude')
                 longitude = parse_qs(parsed_path.query).get('longitude')
-                print("Received request with latitude=" + str(latitude) + " and longitude=" + str(longitude))
                 data = fetchWeather(latitude, longitude) #get json data from the api request
-                if data:
-                    response = json.dumps(data).encode('utf-8')
-                    self.send_response(200) #Successful request
-                    self.send_header('Content-Type', 'application/json')
-                    self.send_header('Content-Length', len(response))
-                    self.end_headers()
-                    self.wfile.write(response)
-                
-            self.send_header('Content-Type', 'text/plain; charset=utf-8')
-            self.end_headers()
+                responseToClient(self, data) if data else print('Data unavailable') #Send data back to client if it's available
+            else:
+                self.send_response(400) #Unsuccessful request, missing parameters
             
     server_address = ('', 5000)
     httpd = HTTPServer(server_address, RequestHandler)
 
     print('Server listening on port 5000')
     httpd.serve_forever()#listen on port 5000
+
+#This function uses the data object that is recieved from the api call
+#To respond to the client with the successful weather response
+def responseToClient(self, data):
+    response = json.dumps(data).encode('utf-8')
+    self.send_response(200) #Successful request
+    self.send_header('Content-Type', 'application/json')
+    self.send_header('Content-Length', len(response))
+    self.end_headers()
+    self.wfile.write(response)
+    
+    self.send_header('Content-Type', 'text/plain; charset=utf-8')
+    self.end_headers()
         
 #This function is called after a successful request is sent to the server 
 #It accepts the latitued and longitude values to make a request to the
@@ -42,14 +47,13 @@ def fetchWeather(lat, lon):
 
     res = requests.get(url, params=params)
     
-    if res.status_code == 200:
+    if (res.status_code == 200):
+        #Successful api call
         data = json.loads(res.content)
         return data
     else:
+        #Unsuccessful api call
         print("Request failed with status code {res.status_code}")
-    
-def main():
-    server()
 
 if __name__ == "__main__":
-    main()
+    server()
